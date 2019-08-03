7ec666e3e6e296e5a8b3dc79dee4b64022f80b6b
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-03 13:49:49 -0400

    changed home page to neighborhood page

--- controllers/neighborhood.js
+++ controllers/neighborhood.js
@@ -4,12 +4,12 @@ const Neighborhood = require('../models/neighborhoods.js');
 const manyNeighborhoods = require('../models/neighborhoodData.js');
 
 // ========== ROUTES ========== //
-neighborhood.get('/' , (req, res) => {
-  res.render('index.ejs')
-});
+// neighborhood.get('/' , (req, res) => {
+//   res.render('index.ejs')
+// });
 
 //get db of neighborhoods
-neighborhood.get('/neighborhoods', (req, res) => {
+neighborhood.get('/undertheradar', (req, res) => {
   Neighborhood.find({}, (error, allNeighborhoods) => {
     res.render('neighborhood/index.ejs',
       {
