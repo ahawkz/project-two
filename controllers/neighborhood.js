9b359667de17c6e4b08295abe3b8f0fef02a7b18
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-03 14:09:13 -0400

    show page renders

--- controllers/neighborhood.js
+++ controllers/neighborhood.js
@@ -4,17 +4,23 @@ const Neighborhood = require('../models/neighborhoods.js');
 const manyNeighborhoods = require('../models/neighborhoodData.js');
 
 // ========== ROUTES ========== //
-// neighborhood.get('/' , (req, res) => {
-//   res.render('index.ejs')
-// });
 
-//get db of neighborhoods
+//get db of neighborhoods on homepage
 neighborhood.get('/undertheradar', (req, res) => {
   Neighborhood.find({}, (error, allNeighborhoods) => {
     res.render('neighborhood/index.ejs',
       {
         neighborhoods: allNeighborhoods
       })
+  })
+});
+
+// show individual neighborhoods
+neighborhood.get('/undertheradar/:id', (req, res) => {
+  Neighborhood.findById(req.params.id, (err, foundNeighborhood) => {
+    res.render('neighborhood/show.ejs', {
+      neighborhood: foundNeighborhood
+    })
   })
 });
 
