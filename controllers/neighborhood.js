cb9490b1e1cc6dd6ae7981968233ed95748f088d
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-04 14:22:24 -0400

    can successfully edit and update restaurant information

--- controllers/neighborhood.js
+++ controllers/neighborhood.js
@@ -17,7 +17,7 @@ neighborhood.get('/undertheradar', (req, res) => {
   })
 });
 
-//new
+//new restaurant
 neighborhood.get('/undertheradar/:id/new', (req, res) => {
    Neighborhood.findById(req.params.id, (err, neighborhood) => {
        res.render('neighborhood/new.ejs', {
@@ -26,23 +26,23 @@ neighborhood.get('/undertheradar/:id/new', (req, res) => {
    })
 })
 
-
-
-//show restaurant information
+//show restaurant information (restaurant.ejs page)
 neighborhood.get('/undertheradar/restaurant/:id', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurantInfo) => {
-    res.render('neighborhood/restaurant.ejs', {
-      restaurant: restaurantInfo
+    // Neighborhood.findById(req.params.neighborhoodId, (err, foundNeighborhood) => {
+    //   console.log(req.params.neighborhoodId);
+      res.render('neighborhood/restaurant.ejs', {
+        restaurant: restaurantInfo,
+      //   neighborhood: foundNeighborhood
+      // })
     })
   })
 });
 
-
-// show individual neighborhoods
+// show individual neighborhoods (show.ejs page)
 neighborhood.get('/undertheradar/:id', (req, res) => {
   Neighborhood.findById(req.params.id, (err, foundNeighborhood) => {
     Restaurant.find({}, (err, foundRestaurants) => {
-      console.log(foundRestaurants);
       res.render('neighborhood/show.ejs', {
         neighborhood: foundNeighborhood,
         restaurant: foundRestaurants
@@ -63,14 +63,30 @@ neighborhood.post('/undertheradar/:id', (req, res) => {
   })
 });
 
+//update existing restaurant restaurant
+ neighborhood.put('/undertheradar/restaurant/:id', (req, res) => {
+   Restaurant.findByIdAndUpdate(req.params.id, req.body, {new:true}, () => {
+     res.redirect('/undertheradar/restaurant/' + req.params.id)
+   })
+ })
+
+
+//delete existing restaurant
+neighborhood.delete('/undertheradar/restaurant/:id', (req, res) => {
+  Restaurant.findByIdAndRemove(req.params.id, (err, restaurant) => {
+    res.redirect('/undertheradar/');
+  })
+})
+
 //Edit existing restaurant
 neighborhood.get('/undertheradar/:id/edit', (req, res) => {
   Restaurant.findById(req.params.id, (err, foundRestaurant) => {
       res.render('neighborhood/edit.ejs', {
-      restaurant: foundRestaurant
+        restaurant: foundRestaurant
     })
   })
 });
+
 
 
 // ========== EXPORT ========== //
